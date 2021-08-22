97dc59c3274bc926447db512a0c640589e32cf3c
ayoj mayor <joycemayor12outlook.com>
2021-08-22 16:58:41 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+    var customerName = 'bob'
+    
+   
+
+    function upperCaseCustomerName(){
+     customerName.upperCaseCustomerName()
+     return customerName
+    }
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = " Sarah"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = " Joy"
+}
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase();
+ 
+}
